57e714c95de13184e56b58fc91967aa0be5ae342
gaiz <594277665@qq.com>
2020-01-16 20:32:04 +0800

    底部制作完成

--- pages/index.vue
+++ pages/index.vue
@@ -13,21 +13,7 @@ export default {
 </script>
 
 <style lang='less' scoped>
-*{
-  margin:0; 
-  padding:0;
-}
-ul, li, ol{
-  list-style:none;
-}
-a{
-  text-decoration:none;
-  color:inherit;
-}
 
-em,i{
-  font-style: normal;
-}
 
 
 </style>
